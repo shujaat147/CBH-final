415f5541cbf788d9731d2fd89b6dc69975265dcd
Shujaat Hussain <shujaathussain147@gmail.com>
2023-10-17 00:30:54 +0500

    2nd Commit

--- js/header.js
+++ js/header.js
@@ -1,8 +1,27 @@
-function closeNavbar() {
- var navbarToggler = document.querySelector('.navbar-toggler');
- var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+document.addEventListener("DOMContentLoaded", function () {
+  const featuresNavLink = document.querySelector(".nav-link.dropdown-toggle");
+  const dropdownMenu = document.querySelector(".dropdown-menu");
 
- if (windowWidth <= 1199.98 && !navbarToggler.classList.contains('collapsed')) {
-  navbarToggler.click();
- }
-}
+  let clickCount = 0;
+
+  featuresNavLink.addEventListener("click", function (event) {
+    if (window.innerWidth < 1200) {
+      if (clickCount === 0) {
+        event.preventDefault();
+        dropdownMenu.style.display = "block";
+      } else {
+        window.location.href = event.target.href;
+      }
+      clickCount++;
+    } else {
+      window.location.href = event.target.href;
+    }
+  });
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth >= 1200) {
+      dropdownMenu.style.display = "none";
+      clickCount = 0;
+    }
+  });
+});
